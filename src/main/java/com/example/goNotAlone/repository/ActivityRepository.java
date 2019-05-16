ac51afd8d9d6a67906ed2099cb72ba073b7a9335
elvira <aelvira312@gmail.com>
2019-05-16 15:45:49 +0600

    create ActivityRepository

---
+++ src/main/java/com/example/goNotAlone/repository/ActivityRepository.java
@@ -0,0 +1,6 @@
+package com.example.goNotAlone.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ActivityRepository extends JpaRepository<ActivityRepository,Long> {
+}
