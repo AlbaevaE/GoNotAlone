3de0f7c093ae35a12c73076627b21d6b5f8d4713
elvira <aelvira312@gmail.com>
2019-05-16 15:15:23 +0600

    create Suggest model

---
+++ src/main/java/com/example/goNotAlone/model/Suggest.java
@@ -0,0 +1,28 @@
+package com.example.goNotAlone.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "suggest")
+public class Suggest {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @OneToMany
+    @JoinColumn(name = "user_id")
+    private User user;
+    @OneToMany
+    @JoinColumn(name = "place_id")
+    private Place place;
+    @OneToMany
+    @JoinColumn(name = "activity_id")
+    private Activity activity;
+
+}
