c969395eec57b7aae4f368c57c0cf942ff914e6d
elvira <aelvira312@gmail.com>
2019-05-18 14:25:27 +0600

    create UserServiceImpl        PlaceServiceImpl        ActivityServiceImpl

---
+++ src/main/java/com/example/goNotAlone/service/PlaceServiceImpl.java
@@ -0,0 +1,39 @@
+package com.example.goNotAlone.service;
+
+import com.example.goNotAlone.repository.GenericRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PlaceServiceImpl implements GenericService {
+    @Autowired
+    private GenericRepository genericRepository;
+
+
+    @Override
+    public Object addG(Object o) {
+        return this.genericRepository.save(o);
+    }
+
+    @Override
+    public Object getById(Long id) {
+        return this.genericRepository.findById(id).get();
+    }
+
+    @Override
+    public List getAll() {
+        return this.genericRepository.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        this.genericRepository.deleteById(id);
+    }
+
+    @Override
+    public void deleteAll() {
+        this.genericRepository.deleteAll();
+    }
+}
