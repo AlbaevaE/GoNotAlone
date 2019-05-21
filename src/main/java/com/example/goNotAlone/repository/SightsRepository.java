24a8fd4c1918b6ed0428c520e48491ae2af116e9
elvira <aelvira312@gmail.com>
2019-05-21 20:11:54 +0600

    create PlaceRepository        SightsRepository        SuggestRepository

---
+++ src/main/java/com/example/goNotAlone/repository/SightsRepository.java
@@ -0,0 +1,7 @@
+package com.example.goNotAlone.repository;
+
+import com.example.goNotAlone.model.Sights;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SightsRepository extends JpaRepository<Sights,Long> {
+}
---
+++ src/main/java/com/example/goNotAlone/repository/SuggestRepository.java
@@ -0,0 +1,7 @@
+package com.example.goNotAlone.repository;
+
+import com.example.goNotAlone.model.Suggest;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SuggestRepository extends JpaRepository<Suggest,Long> {
+}
