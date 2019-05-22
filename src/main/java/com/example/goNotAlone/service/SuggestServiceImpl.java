0dee252b77ecb0470799b7a6480b9b836c213413
elvira <aelvira312@gmail.com>
2019-05-22 17:55:08 +0600

    change implements GenericService -> GenericService<Suggest>

--- src/main/java/com/example/goNotAlone/service/SuggestServiceImpl.java
+++ src/main/java/com/example/goNotAlone/service/SuggestServiceImpl.java
@@ -1,41 +1,40 @@
 package com.example.goNotAlone.service;
 
-import com.example.goNotAlone.model.Activity;
+import com.example.goNotAlone.model.Suggest;
+import com.example.goNotAlone.repository.SuggestRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
-public class SuggestServiceImpl implements GenericService {
+public class SuggestServiceImpl implements GenericService<Suggest> {
     @Autowired
-    private GenericRepository genericRepository;
+    private SuggestRepository suggestRepository;
 
 
     @Override
-    public Activity addG(Object o) {
-        return this.genericRepository.save(o);
+    public Suggest addG(Suggest suggest) {
+        return this.suggestRepository.save(suggest);
     }
 
     @Override
-    public Activity getById(Long id) {
-        return this.genericRepository.findById(id).get();
+    public Suggest getById(Long id) {
+        return this.suggestRepository.findById(id).get();
     }
 
     @Override
-    public List getAll() {
-        return this.genericRepository.findAll();
+    public List<Suggest> getAll() {
+        return this.suggestRepository.findAll();
     }
 
     @Override
     public void deleteById(Long id) {
-        this.genericRepository.deleteById(id);
-
+        this.suggestRepository.deleteById(id);
     }
 
     @Override
     public void deleteAll() {
-        this.genericRepository.deleteAll();
-
+        this.suggestRepository.deleteAll();
     }
 }
