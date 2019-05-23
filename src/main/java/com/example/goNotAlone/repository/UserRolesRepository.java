6ac0b362179af8ea169998ea8304ed12cc81d822
elvira <aelvira312@gmail.com>
2019-05-23 17:11:24 +0600

    create UserRolesRepository change UserRolesServiceImpl

---
+++ src/main/java/com/example/goNotAlone/repository/UserRolesRepository.java
@@ -0,0 +1,7 @@
+package com.example.goNotAlone.repository;
+
+import com.example.goNotAlone.model.UserRoles;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRolesRepository extends JpaRepository<UserRoles,Long> {
+}
--- src/main/java/com/example/goNotAlone/service/UserRolesServiceImpl.java
+++ src/main/java/com/example/goNotAlone/service/UserRolesServiceImpl.java
@@ -1,42 +1,41 @@
-//package com.example.goNotAlone.service;
-//
-//import com.example.goNotAlone.model.Event;
-//import com.example.goNotAlone.model.UserRoles;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Service;
-//
-//import java.util.List;
-//
-//@Service
-//public class UserRolesServiceImpl implements GenericService<UserRoles> {
-//    @Autowired
-//    private UserRoles;
-//
-//
-//    @Override
-//    public Event addG(Object o) {
-//        return this.genericRepository.save(o);
-//    }
-//
-//    @Override
-//    public Event getById(Long id) {
-//        return this.genericRepository.findById(id).get();
-//    }
-//
-//    @Override
-//    public List getAll() {
-//        return this.genericRepository.findAll();
-//    }
-//
-//    @Override
-//    public void deleteById(Long id) {
-//        this.genericRepository.deleteById(id);
-//
-//    }
-//
-//    @Override
-//    public void deleteAll() {
-//        this.genericRepository.deleteAll();
-//
-//    }
-//}
+package com.example.goNotAlone.service;
+
+import com.example.goNotAlone.model.Event;
+import com.example.goNotAlone.model.UserRoles;
+import com.example.goNotAlone.repository.UserRolesRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserRolesServiceImpl implements GenericService<UserRoles> {
+    @Autowired
+    private UserRolesRepository userRolesRepository;
+
+
+    @Override
+    public UserRoles addG(UserRoles userRoles) {
+        return this.userRolesRepository.save(userRoles);
+    }
+
+    @Override
+    public UserRoles getById(Long id) {
+        return this.userRolesRepository.findById(id).get();
+    }
+
+    @Override
+    public List<UserRoles> getAll() {
+        return this.userRolesRepository.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        this.userRolesRepository.deleteById(id);
+    }
+
+    @Override
+    public void deleteAll() {
+        this.userRolesRepository.deleteAll();
+    }
+}
